a3c784a8e37cb72a54788397947cd2c053e7476a
Andrew Hejl <andrew.hejl@outlook.com>
2021-03-11 12:57:15 -0800

    Add project files.

---
+++ GuessNumber.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EX_9A_Bisection_Algorithm
+{
+    class GuessNumber
+    {
+        private static Random rand = new Random();
+        private static int numTimes = 0;
+        public static void HumanPlays()
+        {
+            int number = rand.Next(0,1001);
+            int guess = 0;
+
+            Console.WriteLine($"I'm thinking of a number between 0 and 1000, can you guess what it is?");
+            guess = Int32.Parse(Console.ReadLine());
+
+            while (guess != number)
+            {
+                Console.WriteLine($"{guess} is {( guess > number ? "higher" : "lower" ) } than my number.");
+                Console.WriteLine("Guess again: ");
+                guess = Int32.Parse(Console.ReadLine());
+                numTimes++;
+            }
+
+            Console.WriteLine($"You got it! My number was {number}. It took you {numTimes} guesses.");
+            numTimes = 0;
+        }
+        public static void ComputerPlays()
+        {
+            string LH = "";
+            bool correct = false;
+            int guess = 0;
+            int min = 0;
+            int max = 100;
+
+            Console.WriteLine($"Think of a number between 0 and 100. I'll guess what it is! Press any key to start.");
+            Console.ReadKey(true);
+
+            while (correct == false)
+            {
+                guess = rand.Next(min, max + 1);
+                Console.WriteLine($"Is your number {guess}? If not, am I too high or low? (Enter Y / H / L)");
+                while(LH != "H" && LH != "L" && LH != "Y")
+                {
+                    LH = Console.ReadLine();
+                    LH = LH.ToUpper();
+                }
+                
+                if(LH == "Y")
+                {
+                    correct = true;
+                }
+                if (LH == "H")
+                {
+                    max = guess - 1;
+                }
+                if (LH == "L")
+                {
+                    min = guess + 1;
+                }
+                LH = "";
+                numTimes++;
+            }
+
+            Console.WriteLine($"I got it! Your number was {guess}. It took me {numTimes} guesses.");
+            numTimes = 0;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using static EX_9A_Bisection_Algorithm.GuessNumber;
+
+namespace EX_9A_Bisection_Algorithm
+{
+    class Program
+    {
+        public static int numCount = 0;
+        public static void PrintArray(int[] input)
+        {
+            Console.WriteLine("Current Range:");
+            for (int i = 0; i < input.Length - 1; i++)
+            {
+                Console.Write($"{input[i]}, ");
+            }
+            Console.WriteLine($"{input[input.Length - 1]}.");
+        }
+        public static int Bisection(int num, int[] numList)
+        {
+            PrintArray(numList);
+            int mid = ( numList.Length ) / 2;
+
+
+            int[] newArr = new int[mid];
+
+            if (numList.Length == 1 && numList[0] != num) 
+            {
+                Console.WriteLine($"Value {num} not in list.");
+                return 0;
+            }
+
+            if (numList.Length % 2 == 1) {
+                if (num == numList[mid])
+                {
+                    Console.WriteLine($"Value {num} found. Iterations: {numCount}.");
+                    numCount = 0;
+                    return num;
+                }
+                if (num > numList[mid])
+                {
+                    Console.WriteLine($"Number {num} is larger than middle value {numList[mid]}.");
+                    for (int i = mid + 1; i < numList.Length; i++)
+                    {
+                        newArr[i - mid - 1] = numList[i];
+                    }
+                }
+                if (num < numList[mid])
+                {
+                    Console.WriteLine($"Number {num} is larger than middle value {numList[mid]}.");
+                    for (int i = 0; i < mid; i++)
+                    {
+                        newArr[i] = numList[i];
+                    }
+                }
+            }
+
+            if (numList.Length % 2 == 0)
+            {
+                if (num == numList[mid - 1])
+                {
+                    Console.WriteLine($"Value {num} found.");
+                    return num;
+                }
+                if (num > numList[mid - 1])
+                {
+                    Console.WriteLine($"Number {num} is larger than middle value {numList[mid - 1]}.");
+                    for (int i = mid; i < numList.Length; i++)
+                    {
+                        newArr[i - mid] = numList[i];
+                    }
+                }
+                if (num < numList[mid - 1])
+                {
+                    Console.WriteLine($"Number {num} is larger than middle value {numList[mid - 1]}.");
+                    for (int i = 0; i < mid; i++)
+                    {
+                        newArr[i] = numList[i];
+                    }
+                }
+            }
+            numCount++;
+            return Bisection(num, newArr);
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                int[] list = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+                
+                Console.WriteLine("Enter a value to search in the list:");
+                int userNum = Int32.Parse(Console.ReadLine());
+                
+                
+                Bisection(userNum, list);
+                HumanPlays();
+                ComputerPlays();
+            }
+            catch(Exception EX)
+            {
+                Console.WriteLine(EX.Message);
+            }
+        }
+    }
+}
